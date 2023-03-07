d534ee276a41ff2523f451a8ca2aa6b021084cb5
isra-fs <isra.fsol@gmail.com>
2023-03-06 18:31:16 -0800

    About added

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const About = () =>{
     return(
-        <section id="About">
-           ¿Quiénes somos?
+        <section id="About" className='section-about'>
+             <div className='container'>
+                <h1>Nuestra experiencia</h1>
+                <div className='about-image'>
+                    <img src='resources/images/FotoDoctora.Nosotros.png' alt=''/>
+                </div>
+                <div className='about-description'>
+                    <h2>Dra. Karla Bonilla</h2>
+                    <p>Somos un grupo de especialistas en Odontología
+                        Pediátrica con más de 15 años de experiencia en
+                        la atención de los más pequeños.
+                        En constante actualización, buscando la mejor
+                        tecnología y todas las alternativas de atención
+                        para lograr la mejor experiencia dentro de
+                        nuestra consulta, haciendo agradable y
+                        memorable las visitas al dentista.</p>
+                </div>
+             </div>
         </section>
     )
 }
--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -5,9 +5,20 @@ const TopHeader = () =>{
 
     return(
         <div className='top-header'>
-            <img src='resources/images/socialmedia-facebook.svg'alt='ocialmedia-facebook'/>
-            <img src='resources/images/socialmedia-instagram.svg' alt='ocialmedia-instagram'/>
-            <img src='resources/images/socialmedia-youtube.svg' alt='ocialmedia-youtube'/>
+            <a href='https://www.facebook.com/DraKarlaBonilla' target={'_blank'} rel="noreferrer">
+                <img src='resources/images/socialmedia-facebook.svg'alt='socialmedia-facebook'/>
+            </a>
+            <a href='https://www.instagram.com/dra._karla_bonilla/' target={'_blank'} rel="noreferrer">
+                <img src='resources/images/socialmedia-instagram.svg' alt='socialmedia-instagram'/>
+            </a>
+            <a href='https://www.youtube.com/channel/UCIngEV2uWOW0DMgPsCXYrfA' 
+            target={'_blank'} rel="noreferrer">
+                <img src='resources/images/socialmedia-youtube.svg' alt='socialmedia-youtube'/>
+            </a>
+            <a href='https://www.google.com.mx/maps/place/Dental+Pedi%C3%A1trica%2F+Dra.+Karla+Bonilla/@32.5246159,-117.0153247,17z/data=!3m1!4b1!4m5!3m4!1s0x80d9499106d91f1b:0xa6552d2c22c977ed!8m2!3d32.5246119!4d-117.0131368?hl=es&shorturl=1'
+             target={'_blank'} rel="noreferrer">
+                <img src='resources/images/socialmedia-googlemaps.svg' 
+             alt='socialmedia-map'/></a>
         </div>
     )
 }
